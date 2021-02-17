205a30fdcfad2adfb135174b3efdc923a2872c4d
sdsd2131 <p31982522@gmail.com>
2021-02-17 18:19:52 +0900

    상세페이지 삭제기능 추가

--- src/main/java/impl/ViewImpl.java
+++ src/main/java/impl/ViewImpl.java
@@ -16,42 +16,17 @@ public interface ViewImpl {
 	//상세페이지
 	public AudioBoardDTO View(int audio_idx);
 	
+	//상세페이지 삭제처리
+	public int viewDelete(int audio_idx, String id);
+	
 	//댓글리스트
 	public ArrayList<MCommentDTO> mComment(int audio_idx);
 	
 	//댓글추가
-	public int commentAction(
-		@Param("audio_idx") int audio_idx,
-		@Param("id") String id,
-		@Param("contents") String contents);
+	public int commentAction(int audio_idx, String id, String contents);
 	
-	//데이터 삭제
-	public int delete(@Param("comment_idx") int comment_idx,
-		@Param("id") String id);
-	
-	//기존게시물 조회
-	public AudioBoardDTO modify(@Param("audio_idx") int audio_idx,
-		@Param("id") String id);
-	
-	//게시물 수정하기
-	public int modifyAction(String audiotitle, String aritstname,
-		String contents, String audiofilename, String imagename,
-		String category, int party, int audio_idx, String id);
-	
-	//게시물 수정하기(오디오파일X)
-	public int modifyAction2(String audiotitle, String artistname,
-		String contents, String imagename, String category, int party,
-		int audio_idx, String id);
-	
-	//게시물 수정하기(이미지파일X)
-	public int modifyAction3(String audiotitle, String artistname,
-		String contents, String audiofilename, String category, int party,
-		int audio_idx, String id);
-	
-	//게시물 수정하기(오디오, 이미지X)
-	public int modifyAction4(String audiotitle, String artistname,
-		String contents, String category, int party,
-		int audio_idx, String id);
+	//댓글 삭제
+	public int delete(int comment_idx);
 	
 	//협업자 불러오기
 	public ArrayList<PartyBoardDTO> partyMember(int audio_idx);
